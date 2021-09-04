dd8913584970a359f601f7844a3cfce6cdf495a1
Moradi1412 <moradi.javid@gmail.com>
2021-09-04 16:43:14 -0700

    adding fetch code to display the current

--- assets/Java/homepage.js
+++ assets/Java/homepage.js
@@ -1,26 +1,51 @@
-var getCurrentWeather = function () {
+//DOM ELEMENT 
+var submitButtom = document.getElementById("city-form"); 
+var currentTempEL = document.getElementById("temperature"); 
+var currentWindSpeed = document.getElementById ("wind-speed"); 
+var currentHumidity = document.getElementById ("humidity"); 
+var uvIndex = document.getElementById ("uv-index"); 
 
-    
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Sacramento&appid=fd0ea7baffebc408619b7fb5206660c9")
+var getCityName = function (event) {
+    event.preventDefault(); 
+    var cityName = document.querySelector("#cityname").value; 
+
+    if (cityName) {
+        getCurrentWeather(cityName); 
+    } else {
+        alert ("please enter name of a city")
+    } 
+}; 
+
+
+
+var getCurrentWeather = function (cityName) {      
+   
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName +"&units=imperial&appid=fd0ea7baffebc408619b7fb5206660c9")
         .then(function (response) {
-            // request was successful
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
+                    displayCurrent(data);
                 });
             } else {
                 alert('Error: GitHub User Not Found');
             }
         })
         .catch(function (error) {
-            // Notice this `.catch()` getting chained onto the end of the `.then()` method
             alert("Unable to connect to GitHub");
         });
 };
 
-getCurrentWeather();
 
-var displayCurrent = function (){ 
+submitButtom.addEventListener("submit", getCityName); 
 
+var displayCurrent = function (data){ 
+    console.log(data);
+    var currentTemp = data.main.temp ;
+    var currentHumd = data.main.humidity; 
+    var currentWind = data.wind.speed; 
+    
+    currentTempEL.innerHTML = currentTemp + " F";
+    currentHumidity.innerHTML = currentHumd + " %"; 
+    currentWindSpeed.innerHTML = currentWind + " MPH"; 
 }
