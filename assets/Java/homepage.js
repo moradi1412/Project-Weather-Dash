9210e09b49c89ebf5295280ee9471bb4e916f17d
Moradi1412 <moradi.javid@gmail.com>
2021-09-06 21:42:13 -0700

    local bug issue

--- assets/Java/homepage.js
+++ assets/Java/homepage.js
@@ -13,24 +13,23 @@ var cities = [];
 
 
 var getCityName = function (event) {
-
     event.preventDefault(); 
     var cityName = document.querySelector("#cityname").value; 
-
     if (cityName) {
         getCurrentWeather(cityName);  
         cities.push(cityName); 
         saveSearch();
+        displaySearchHistory();
     } else {
         alert ("please enter name of a city")
-    } 
-      
+    }
+       
 }; 
+
 
 var saveSearch = function (){
     localStorage.setItem("cities", JSON.stringify(cities)); 
 }
-
 
 var getCurrentWeather = function (cityName) { 
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName +"&units=imperial&appid=" + APIKey)
@@ -58,7 +57,6 @@ var displayCurrent = function (data, cityName)
     var currentDate = moment().format('dddd MMM Do, YYYY'); 
     $("#currentDay").html(cityName + " : " +   currentDate); 
     currentImage.setAttribute('src', 'http://openweathermap.org/img/wn/'+ data.weather[0].icon + '@2x.png' ); 
-   
     var currentTemp = data.main.temp ;
     var currentHumd = data.main.humidity; 
     var currentWind = data.wind.speed; 
@@ -89,21 +87,22 @@ var fiveDayforecase = function (lat, lon) {
                 displayFive(data); 
             })
         }
-    })};
+})
+};
 
 
 var displayFive = function (data) { 
     console.log(data); 
-    for (let i = 0; i < 5 ; i++) {
+    // clearing the fiveday elment
+    $("#foreCast").html("");
 
-
+    for (let i = 0; i < 5 ; i++) {    
 
     var fiveDaysEl = document.createElement("div"); 
-    fiveDaysEl.classList = "container col-md-2 card"; 
-
+    fiveDaysEl.classList = "container col-md-2 card fivedays"; 
   
     var new_date = document.createElement("span"); 
-    new_date.classList= "card body"; 
+    new_date.classList= "card-body"; 
     new_date.textContent = moment.unix(data.daily[i + 1 ].dt).format("MMM D, YYYY");
     fiveDaysEl.appendChild(new_date);
 
@@ -116,40 +115,52 @@ var displayFive = function (data) {
 
     var Tempture = document.createElement("span"); 
     Tempture.classList = "card-body"; 
-    Tempture.textContent ="Temperature :  " +  data.daily[i].temp.day + "  F"; 
+    Tempture.textContent ="Temp :  " +  data.daily[i].temp.day + "  F"; 
     fiveDaysEl.appendChild(Tempture);
 
     var windSpeed = document.createElement("span"); 
     windSpeed.classList = "card-body"; 
-    windSpeed.textContent ="Wind-Speed :  " +  data.daily[i].wind_speed + "  MPH"; 
+    windSpeed.textContent ="W-S :  " +  data.daily[i].wind_speed + "  MPH"; 
     fiveDaysEl.appendChild(windSpeed); 
     
 
     var Humidity = document.createElement("span"); 
     Humidity.classList = "card-body"; 
-    Humidity.textContent ="Humidity :  " +  data.daily[i].humidity + "  %"; 
-    fiveDaysEl.appendChild(Humidity);
-          
+    Humidity.textContent ="Humid :  " +  data.daily[i].humidity + "  %"; 
+    fiveDaysEl.appendChild(Humidity);          
 
     foreCastEl.appendChild(fiveDaysEl);
 }
 };
 
 var displaySearchHistory = function () { 
+    $("#searchHistory").html("");
     var citiesName = localStorage.getItem("cities"); 
     var citynames = JSON.parse(citiesName); 
-    console.log(citynames); 
+    if (citynames)
+    {
     for (let i = 0; i < citynames.length; i++) {
         var searchHistoryEl = document.createElement('button'); 
-        searchHistoryEl.classList = "btn-light searchHistory";
+        searchHistoryEl.classList = "d-flex w-100 btn-light border p-2";
         searchHistoryEl.setAttribute ("type", "submit"); 
         searchHistoryEl.textContent = citynames[i]; 
-        searchHistory.appendChild(searchHistoryEl);        
-    }  
+        searchHistoryEl.setAttribute ("city-data", citynames[i]);
+        searchHistory.appendChild(searchHistoryEl);    
+    }
+    }
+
+    searchHistoryEl.addEventListener("click", function () {
+        var city = searchHistoryEl.textContent; 
+        console.log(city);
+        getCurrentWeather(city); 
+      
+    })
 }; 
+ 
 
 
-displaySearchHistory();
+
+
 
 
 submitButtom.addEventListener("submit", getCityName); 
